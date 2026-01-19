1fc7fb538f5c71c5b53e7f79f75a0da07343d7c5
Khanh091 <nvkhanh0911@gmail.com>
2026-01-19 21:23:30 +0700

    fix login, view list book

--- store/controllers/staffController/views.py
+++ store/controllers/staffController/views.py
@@ -3,10 +3,10 @@ from store.models import Book
 
 def staff_add_book(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        author = request.POST['author']
-        price = request.POST['price']
-        stock = request.POST['stock']
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        price = request.POST.get('price')
+        stock = request.POST.get('stock')
         Book.objects.create(title=title, author=author, price=price, stock_quantity=stock)
         return redirect('book_list')
     return render(request, 'staff/add_book.html')
