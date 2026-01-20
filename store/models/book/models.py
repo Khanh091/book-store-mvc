a4739ad0ac70a2826c2db6d450ba604d432c85cf
Khanh091 <nvkhanh0911@gmail.com>
2026-01-20 18:38:59 +0700

    config supabase, change script sql

--- store/models/book/models.py
+++ store/models/book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import json
 
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,3 +10,10 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    embedding = models.TextField(blank=True, null=True)  # Lưu vector as JSON string: '[0.1, 0.2, ...]'
+
+    def set_embedding(self, vector):
+        self.embedding = json.dumps(vector.tolist()) if isinstance(vector, np.ndarray) else json.dumps(vector)
+
+    def get_embedding(self):
+        return json.loads(self.embedding) if self.embedding else None
